825c763a06aac08f43a4b1844537a701db0d1df3
Eliab Lemus <2540996610101@ingenieria.usac.edu.gt>
2022-06-20 00:00:15 -0600

    carpetas para consultas y conexiones separadas, tablas temporales creadas

---
+++ Aplicacion/sqlserver/statements.py
@@ -0,0 +1,39 @@
+from ast import Try
+from typing import final
+import pymssql
+import config
+from sqlserver.queries import * 
+conn = pymssql.connect(host=config.sql_server, user=config.sql_user, password=config.sql_password, database=config.sql_database)
+#conexion a sql server
+class StatementsSQL(object):
+    def __init__(self):
+        pass
+    def delete_temporal_table(self):
+        try:
+            cursor = conn.cursor()
+            cursor.execute(DROP_TABLES)
+            conn.commit()
+        except Exception as e:
+            raise e from Exception
+        # finally:
+            # conn.close()
+
+    def build_temporal_table(self):
+        try:
+            cursor = conn.cursor()
+            cursor.execute(TEMPORAL_CREATION)
+            conn.commit()
+            # cursor.execute(COLLATE)
+        except Exception as e:
+            raise e from Exception
+        # finally:
+            # conn.close()
+    
+    def load_temporal_pib(self,df):
+        try:
+            cursor = conn.cursor()
+            for row in df:
+                cursor.execute(row)
+            conn.commit()
+        except Exception as e:
+            raise e from Exception
